a9a4c0cce6671d421db407c41ead7e72b8f4019b
maikytech <maikytech1307@gmail.com>
2022-03-16 12:08:59 -0500

    Start Project

---
+++ TableViewWithCustomCellProgrammatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewWithCustomCellProgrammatically
+//
+//  Created by Maiqui Cedeño on 16/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
